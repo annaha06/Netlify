2e15fb9d63008687e30ffe08c301ee44850aab4e
annaha06 <60203821+annaha06@users.noreply.github.com>
2020-05-11 09:28:13 -0500

    Added to drag and drop.

---
+++ scripts/my-scripts.js
@@ -0,0 +1,10 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    //alert("My Alert");
+  
+    //this is for the carousel. Change interval to manipulate speed  
+    $('.carousel').carousel({
+      interval: 20000
+    });
+  
+});
